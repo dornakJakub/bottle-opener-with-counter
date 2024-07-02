3680531b5a691ef137a4729abbf185529a78097b
Jakub Dornak <dornakjakub9@gmail.com>
2024-07-03 00:28:30 +0200

    display can display integers, sensor value read init

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
+#include <stdio.h>
 
 #define GPIO_ON 1
 #define GPIO_OFF 0
@@ -19,18 +20,20 @@
 #define DIGIT_3 9
 #define DIGIT_4 10
 
+#define SENSOR 11
+
 // 7-segment display numbers representation
 uint8_t numbers[10] = {
-    0b00111111, // 0
-    0b00000110, // 1
-    0b01011011, // 2
-    0b01001111, // 3
-    0b01100110, // 4
-    0b01101101, // 5
-    0b01111101, // 6
-    0b00000111, // 7
-    0b01111111, // 8
-    0b01101111  // 9
+    0b11000000, // 0
+    0b11111001, // 1
+    0b10100100, // 2
+    0b10110000, // 3
+    0b10011001, // 4
+    0b10010010, // 5
+    0b10000010, // 6
+    0b11111000, // 7
+    0b10000000, // 8
+    0b10010000  // 9
 };
 
 // Displays number num on digit digit
@@ -45,19 +48,38 @@ void display_digit(uint8_t digit, uint8_t num) {
     }
 }
 
+void int_to_digits(int num, uint8_t digits[4]) {
+    for (int i = 0; i < 4; i++) {
+        digits[i] = num % 10;
+        num /= 10;
+    }
+}
+
 int main() {
     // Initialize display pins as outputs
+    stdio_init_all();
+    gpio_init(SENSOR);
+    gpio_set_dir(SENSOR, GPIO_IN);
+
     for (int i = 0; i < 11; i++) {
         gpio_init(i);
         gpio_set_dir(i, GPIO_OUT);
     }
 
+    int count = 0;
+    uint8_t digits[4];
+    int_to_digits(count, digits);
+
     while (true) {
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 4; j++) {
-                display_digit(j, i);
-                sleep_ms(1000);
-            }
+        for (int i = 0; i < 4; i++) {
+            display_digit(i, digits[i]);
+            sleep_ms(2);
         }
+
+        int sensor_value = gpio_get(SENSOR);
+
+        printf("Sensor Value: %d\n", sensor_value);
+
+        sleep_ms(200);
     }
 }
